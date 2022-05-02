91653509ec701d2d26c5a40ab558e4bf3b95d88f
Matthew Warman <warmanmr@gmail.com>
2022-05-02 12:08:34 -0400

    winston logger configuration

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,24 +1,10 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
 
-const logger = winston.createLogger({
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: format.combine(format.timestamp(), format.json()),
   defaultMeta: { service: 'todo-service' },
-  transports: [
-    // - write all logs with importance level of `error` or less to `error.log`
-    // - write all logs with importance level of `info` or less to `info.log`
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
+  transports: [new transports.Console()],
 });
-
-// For non-production environments, also log to the `console`
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    }),
-  );
-}
 
 module.exports = logger;
