106cc92775c7ea168d2076a5164a26bc9f93f8ad
Matthew Warman <warmanmr@gmail.com>
2022-05-26 08:36:11 -0400

    token payload

--- src/routes/v1/auth/sign-up.ts
+++ src/routes/v1/auth/sign-up.ts
@@ -9,12 +9,8 @@ export const signUp = async (req: Request, res: Response, next: NextFunction): P
     const accountService = new AccountService();
     const account = await accountService.createOne(req.body);
 
-    // do not return sensitive account attributes
-    const { _id, username } = account;
-    res.send({
-      _id,
-      username,
-    });
+    // TODO: do not return sensitive account attributes
+    res.send(account);
   } catch (err: AccountExistsError | unknown) {
     if (err instanceof AccountExistsError) {
       res.status(409).end();
