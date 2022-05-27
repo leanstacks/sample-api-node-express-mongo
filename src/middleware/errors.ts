fa795c0adc9a3b5d446c77cddb2f17a47fc3e90e
Matthew Warman <warmanmr@gmail.com>
2022-05-27 13:10:08 -0400

    validate sign up

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -2,14 +2,19 @@ import { NextFunction, Request, Response } from 'express';
 import { logger } from '../utils/logger';
 
 export const logErrors = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  logger.error(err.stack);
+  logger.error(`${err.message}`, { name: err.name, stack: err.stack });
   next(err);
 };
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   try {
-    res.status(500);
-    res.end();
+    if (err.name === 'ValidationError') {
+      res.status(409).send({
+        message: err.message,
+      });
+    } else {
+      res.status(500).end();
+    }
   } catch (err) {
     next(err);
   }
