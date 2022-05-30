bbf21823fe1bea663f6f3ea0730d3c481c6b95e8
Matthew Warman <warmanmr@gmail.com>
2022-05-30 07:57:24 -0400

    initial jest setup

---
+++ src/routes/__tests__/health.test.ts
@@ -0,0 +1,18 @@
+import app from '../../app';
+
+import request from 'supertest';
+
+describe('GET /health', () => {
+  it('should return status 200', async () => {
+    const res = await request(app).get('/health');
+
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should return body with status', async () => {
+    const res = await request(app).get('/health');
+
+    expect(res.headers['content-type']).toMatch(/json/);
+    expect(res.body.status).toEqual('UP');
+  });
+});
