10068bb6ed4c2e1aeecf0f6c30e8b886cae76989
Matthew Warman <warmanmr@gmail.com>
2022-05-31 07:47:30 -0400

    updated health tests

--- src/routes/__tests__/health.test.ts
+++ src/routes/__tests__/health.test.ts
@@ -9,10 +9,15 @@ describe('GET /health', () => {
     expect(res.statusCode).toEqual(200);
   });
 
-  it('should return body with status', async () => {
+  it('should return content type "application/json"', async () => {
     const res = await request(app).get('/health');
 
     expect(res.headers['content-type']).toMatch(/json/);
+  });
+
+  it('should return body with status', async () => {
+    const res = await request(app).get('/health');
+
     expect(res.body.status).toEqual('UP');
   });
 });
