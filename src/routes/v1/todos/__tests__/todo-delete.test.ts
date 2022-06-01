72e9e303f86d2aa5f5b1642048a421926a429313
Matthew Warman <warmanmr@gmail.com>
2022-06-01 09:27:02 -0400

    delete todo route tests

---
+++ src/routes/v1/todos/__tests__/todo-delete.test.ts
@@ -0,0 +1,69 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+import AccountService from '../../../../services/account-service';
+import JwtService from '../../../../services/jwt-service';
+import app from '../../../../app';
+
+import TodoService from '../../../../services/todo-service';
+jest.mock('../../../../services/todo-service');
+
+const mockedTodoService = jest.mocked(TodoService);
+
+describe('DELETE /v1/todos/:id', () => {
+  let mongo: MongoMemoryServer;
+  let token: string;
+
+  beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    mongoose.connect(mongo.getUri());
+  });
+
+  beforeEach(async () => {
+    const account = await AccountService.createOne({ username: 'user@example.com', password: 'Iamagoodpassword1!' });
+    token = JwtService.createToken({ account });
+  });
+
+  afterEach(async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+      await collections[key].deleteMany({});
+    }
+
+    mockedTodoService.deleteOne.mockClear();
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+    await mongo.stop();
+  });
+
+  it('should require authentication', async () => {
+    const res = await request(app).delete('/v1/todos/1');
+
+    expect(res.statusCode).toEqual(401);
+  });
+
+  it('should return status code 204', async () => {
+    const res = await request(app).delete('/v1/todos/1').auth(token, { type: 'bearer' });
+
+    expect(res.statusCode).toEqual(204);
+  });
+
+  it('should call return status code 500 when an error occurs', async () => {
+    mockedTodoService.deleteOne.mockRejectedValueOnce(new Error());
+
+    const res = await request(app).delete('/v1/todos/1').auth(token, { type: 'bearer' });
+
+    expect(mockedTodoService.deleteOne).toHaveBeenCalled();
+    expect(res.statusCode).toEqual(500);
+  });
+
+  it('should call TodoService', async () => {
+    const res = await request(app).delete('/v1/todos/1').auth(token, { type: 'bearer' });
+
+    expect(mockedTodoService.deleteOne).toHaveBeenCalled();
+    expect(res.statusCode).toEqual(204);
+  });
+});
--- src/routes/v1/todos/todo-delete.ts
+++ src/routes/v1/todos/todo-delete.ts
@@ -7,7 +7,7 @@ export const deleteTodo = async (req: Request, res: Response, next: NextFunction
   try {
     logger.info('handler::deleteTodo');
 
-    await TodoService.deleteOne(req?.params?.id);
+    await TodoService.deleteOne(req.params.id);
 
     res.status(204).end();
   } catch (err: unknown) {
